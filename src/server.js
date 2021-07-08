cd7255213b051070bf0acfe01b65aa4427eee97b
yonghzhang <yonghzhang@tencent.com>
2021-07-08 19:00:22 +0800

    实现 koa 下的基础版 ssr

--- src/server.js
+++ src/server.js
@@ -1,12 +1,19 @@
 import React from "react";
-import express from "express";
+import path from "path";
+import "regenerator-runtime";
+import Koa from "koa";
+import Router from 'koa-router';
+import mount from 'koa-mount';
+import staticly from 'koa-static';
 import { renderToString } from "react-dom/server";
 import Home from "./pages/Home";
-const app = express();
-app.use(express.static("public"));
-app.get("*", (req, res) => {
+const app = new Koa();
+const router = new Router();
+router.get("/", async ctx => {
   const content = renderToString(<Home text="hello ssr" />);
-  const html = `<html><body>${content}</body></html>`
-  res.send(html);
+  const html = `<html><body>${content}</body></html>`;
+  ctx.body = html;
 });
+app.use(router.routes()).use(router.allowedMethods());
+app.use(mount('/', staticly(path.join(__dirname, './build'), { maxage: 24 * 60 * 60 * 1000 })));
 app.listen(8001, () => console.log("listening on port 8001"));
