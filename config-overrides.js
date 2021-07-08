fb3588f8cdab26cac2941f89727164a9281ed486
yonghzhang <yonghzhang@tencent.com>
2021-07-08 18:34:17 +0800

    修复非 cra 项目的构建错误

--- config-overrides.js
+++ config-overrides.js
@@ -7,19 +7,19 @@ module.exports = {
   webpack: config => {
     config.output.publicPath = './';
     if (isSSR) {
-      config.target = 'node';
-      config.entry = path.resolve(__dirname, 'server/index.js');
-      config.output.filename = 'bundle-server.js';
-      delete config.optimization.splitChunks; // output 不分包
+      config.target = 'node'; // 导出 node 环境代码
+      config.entry = path.resolve(__dirname, 'src/server.js');
+      config.output.filename = 'bundle-server.js'; // output 不分包命名
+      delete config.optimization.splitChunks;
       delete config.optimization.runtimeChunk;
-      if (!config.externals) config.externals = [];
-      const rules = config.module.rules[1].oneOf;
+      const rules = config.module.rules[1].oneOf; // 加入 @babel/preset-react
       rules.forEach(rule => {
         if (!rule.loader || !rule.loader.includes('babel-loader')) return;
         rule.options.presets.push('@babel/preset-react');
       });
+      if (!config.externals) config.externals = []; // 规避不必要的 node_modules 导出
       config.externals.push(webpackNodeExternals());
-      const excludes = ['HtmlWebpackPlugin', 'InlineChunkHtmlPlugin', 'InterpolateHtmlPlugin', 'ManifestPlugin'];
+      const excludes = ['HtmlWebpackPlugin', 'InlineChunkHtmlPlugin', 'InterpolateHtmlPlugin', 'ManifestPlugin']; // 去除不必要的 plugins
       config.plugins = config.plugins.filter(plugin => !excludes.includes(plugin.constructor.name));
     }
     return config;
